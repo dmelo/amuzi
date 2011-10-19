9ae03cc62b51e1554cd59b4f542cd2a8048894bf
Diogo Oliveira de Melo <dmelo87@gmail.com>
2011-10-19 01:09:47 -0200

    fix module issue

---
+++ application/modules/user/controllers/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+class User_IndexController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+
+}
+
