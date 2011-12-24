f778efe3c7583a17767f2c82b6d18468247a6de6
Diogo Oliveira de Melo <dmelo87@gmail.com>
2011-12-24 17:50:06 -0200

    start locale pt_BR

---
+++ locale/pt_BR.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+    'Search' => 'Buscar',
+    'Playlist' => 'Playlist',
+    'About' => 'Sobre',
+
+    'amazingly free' => 'Encrivelmente livre',
+
+    'Login with Facebook' => 'Login do Facebook',
+    'Update Required' => 'Necessário fazer atualização',
+'To play the media you will need to either update your browser to a recent version or update your ' => 'To play the media you will need to either update your browser to a recent version or update your '
+);
