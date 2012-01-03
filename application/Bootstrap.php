6c79e20839c73a2f3b0cfa65b32fb4916188e903
Diogo Oliveira de Melo <dmelo87@gmail.com>
2012-01-03 18:26:23 -0200

    cosmetic changes

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -48,6 +48,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $js[] = $domainJs . 'jquery.form.js';
         $js[] = $domainJs . 'jquery.autocomplete.js';
         $js[] = $domainJs . 'resultset.js';
+        $js[] = $domainJs . 'bootstrap-alerts.js';
         $js[] = $domainJs . 'default.js';
 
         $css = array();
