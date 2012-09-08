772127532b4c295a059e862379bc3ea107f23802
Diogo Oliveira de Melo <dmelo87@gmail.com>
2012-09-07 23:21:17 -0300

    part of the files copyrighted

--- application/controllers/ApiController.php
+++ application/controllers/ApiController.php
@@ -4,10 +4,21 @@
  * ApiController
  *
  * @version 1.0
- * @copyright Copyright (C) 2011 Diogo Oliveira de Melo. All rights reserved.
- * @author Diogo Oliveira de Melo <dmelo87@gmail.com>
- * @license GPL
+ * Amuzi - Online music
+ * Copyright (C) 2010-2012  Diogo Oliveira de Melo
  *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 class ApiController extends DZend_Controller_Action
 {
