fe4b14f5d5aa25a6e608da91eb18573b2c6d835e
Diogo Oliveira de Melo <dmelo87@gmail.com>
2012-09-08 18:21:19 -0300

    automatically inserting the copyright header on php files

---
+++ insertCopyright.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+for file in $@
+do
+    grep "* This program is free software:" $file
+    if [ 1 -eq $? ]
+    then
+        echo "handling $file"
+        head -n 2 $file > $file.tmp
+        cat copyright.aux >> $file.tmp
+        TOTAL=`wc -l $file | cut --delimiter=' ' -f 1`
+        TOTAL=`expr $TOTAL - 2`
+        tail -n $TOTAL $file >> $file.tmp
+        mv $file.tmp $file
+    fi
+
+    grep "* IndexController" $file
+    if [ 0 -eq $? ]
+    then
+        CLASS=`grep "^class" $file | cut --delimiter=' ' -f 2`
+        sed -i "s/\* IndexController/* $CLASS/g" $file
+    fi
+done
