dfd5601361a42b3d95fb9efe67de458659ad47f3
Diogo Oliveira de Melo <dmelo87@gmail.com>
2012-10-27 14:47:27 -0200

    slide is ready

--- public/js/bootstrap-slide.js
+++ public/js/bootstrap-slide.js
@@ -11,6 +11,48 @@
             $(this).css('left', left);
             left += slideWidth;
         });
+
+        $('.slidegroup').append('<div class="slide-next slide-button active"></div>');
+        $('.slidegroup').append('<div class="slide-prev slide-button"></div>');
+
+        $(document).keyup(function (e) {
+            var code = e.keyCode;
+            console.log(code);
+            switch (code) {
+                case 37:
+                    $('.slide-prev').trigger('click');
+                    break;
+                case 39:
+                    $('.slide-next').trigger('click');
+                    break;
+            }
+        });
+
+        $('#screen-search').click(function(e) {
+            $('.slide-prev').trigger('click');
+        });
+
+        $('#screen-music').click(function(e) {
+            $('.slide-next').trigger('click');
+        });
+
+        $('.slide-prev.active').live('click', function(e) {
+            e.preventDefault();
+            $('#screen-search img').attr('src', '/img/search.png');
+            $('#screen-music img').attr('src', '/img/music-dark.png');
+            $.slidePrev();
+        });
+
+        $('.slide-next.active').live('click', function(e) {
+            e.preventDefault();
+            $('#screen-search img').attr('src', '/img/search-dark.png');
+            $('#screen-music img').attr('src', '/img/music.png');
+            $.slideNext();
+        });
+
+        $('#screen-search').html('<img src="/img/search.png"/>');
+        $('#screen-music').html('<img src="/img/music-dark.png"/>');
+
     };
 
     $.slideNext = function() {
@@ -18,6 +60,8 @@
         $('.slide').each(function() {
             var offset = $(this).offset();
             offset.left -= slideWidth;
+            $('.slide-next').removeClass('active');
+            $('.slide-prev').addClass('active');
             $(this).animate(offset);
         });
     };
@@ -27,6 +71,8 @@
         $('.slide').each(function() {
             var offset = $(this).offset();
             offset.left += slideWidth;
+            $('.slide-prev').removeClass('active');
+            $('.slide-next').addClass('active');
             $(this).animate(offset);
         });
     };
