286c0178a94870d82bb25ead84c167f0b335660d
Diogo Oliveira de Melo <dmelo87@gmail.com>
2012-12-24 12:36:25 -0200

    insert proper copyright on JS files

--- insertCopyrightJS.sh
+++ insertCopyrightJS.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-for file in $@
+FILES="bootstrap-slide.js bootstrap-tutorial.js commands.js default.js facebook-connect.js incboard* jquery.bootstrap* jquery.subtitle.js resultset.js share.js"
+
+for file in $FILES
 do
+    file=public/js/${file}
     grep "* This program is free software:" $file
     if [ 1 -eq $? ]
     then
