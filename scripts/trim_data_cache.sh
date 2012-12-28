0dedf1035248cd45cda7bf1125f59a1ae2adfacd
Diogo Oliveira de Melo <dmelo87@gmail.com>
2012-12-28 00:27:56 +0000

    taking the debug part away

--- scripts/trim_data_cache.sh
+++ scripts/trim_data_cache.sh
@@ -7,8 +7,7 @@ while [ $THRESHOLD -le `du $DATAPATH | cut -f 1` ]
 do
     FILE=`ls -t $DATAPATH/*.access | tail -n 1`
     echo $FILE
-    echo "rm -- `echo $FILE | sed 's/\.access//g'`"
-    echo "rm -- $FILE"
-    break
+    rm -- `echo $FILE | sed 's/\.access//g'`
+    rm -- $FILE
 done
 
