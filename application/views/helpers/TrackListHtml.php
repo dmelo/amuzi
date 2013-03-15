f055483219ecf759da6f99c3b1046a96b053ba07
Diogo Oliveira de Melo <dmelo87@gmail.com>
2013-03-15 00:17:37 -0300

    share album feature seems to be ready

---
+++ application/views/helpers/TrackListHtml.php
@@ -0,0 +1,19 @@
+<?php
+
+class View_Helper_TrackListHtml extends Zend_View_Helper_Abstract
+{
+    public function trackListHtml($trackList)
+    {
+        $ret = '<ul>';
+        $count = 1;
+        foreach ($trackList as $track) {
+            $ret .= '<li><img src="/img/play_icon_mini.png"/> ' . $count . ' - '. $track['title'] . '</li>';
+            if ($count++ >= 5) {
+                break;
+            }
+        }
+        $ret .= '</ul>';
+
+        return $ret;
+    }
+}
