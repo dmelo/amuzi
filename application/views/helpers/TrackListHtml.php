d704721f130a0f9b7f4c4fcd03194d98cc2f17d8
Diogo Oliveira de Melo <dmelo87@gmail.com>
2013-04-06 19:32:23 -0300

    small fix

--- application/views/helpers/TrackListHtml.php
+++ application/views/helpers/TrackListHtml.php
@@ -7,9 +7,12 @@ class View_Helper_TrackListHtml extends Zend_View_Helper_Abstract
         $ret = '<ul>';
         $count = 1;
         foreach ($trackList as $track) {
-            $ret .= '<li><img src="/img/play_icon_mini.png"/> ' . $count . ' - '. $track['title'] . '</li>';
-            if ($count++ >= 5) {
-                break;
+            if (array_key_exists('title', $track)) {
+                $ret .= '<li><img src="/img/play_icon_mini.png"/> '
+                    . $count . ' - '. $track['title'] . '</li>';
+                if ($count++ >= 5) {
+                    break;
+                }
             }
         }
         $ret .= '</ul>';
