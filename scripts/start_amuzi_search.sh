2e1e0fadaf2860d12b3d669b4595c4d2c305d44a
Diogo Oliveira de Melo <dmelo87@gmail.com>
2013-06-06 20:59:58 -0300

    script that easily launch amuzi_search on top of gdb

--- scripts/start_amuzi_search.sh
+++ scripts/start_amuzi_search.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-killall -9 amuzi_search
-cd library/amuzi_search/env_album/ ; ../amuzi_search suffixarray ../../../public/data/artist_album.txt 3675 &
+cd library/amuzi_search/env_album/
+
+if [[ $# -eq 1 && "$1" == 'debug' ]]
+then
+    echo 'debug'
+    xterm -fg white -bg black -e 'echo "r suffixarray ../../../public/data/artist_album.txt 3675" | gdb ../amuzi_search' &
+else
+    echo 'prod'
+    ../amuzi_search suffixarray ../../../public/data/artist_album.txt 3675 &
+fi
 cd -
-cd library/amuzi_search/env_track/ ; ../amuzi_search suffixarray ../../../public/data/artist_track.txt 3676 &
+
+cd library/amuzi_search/env_track/
+
+if [[ $# -eq 1 && $1 = 'debug' ]]
+then
+    echo 'debug'
+    xterm -fg white -bg black -e 'echo "r suffixarray ../../../public/data/artist_track.txt 3676" | gdb ../amuzi_search' &
+else
+    echo 'prod'
+    ../amuzi_search suffixarray ../../../public/data/artist_track.txt 3676 &
+fi
