008c4087ba6f437f2e66e60ade9c7d123fd81a81
Diogo Oliveira de Melo <dmelo87@gmail.com>
2013-11-16 04:08:47 -0200

    close #152

--- public/js/default.js
+++ public/js/default.js
@@ -737,7 +737,7 @@
             addElement($(this).parent(), false);
         });
 
-        $('.music-large .play, .music-square .play, .album-square .play, .similarity-list .object-playlist .play').live('click', function (e) {
+        $('.music-large .play, .music-square .play, .album-square .play, .similarity-list .object-playlist .play, .music-square .description').live('click', function (e) {
             e.preventDefault();
             $('.modal .close').trigger('click');
             addElement($(this).parent(), true);
