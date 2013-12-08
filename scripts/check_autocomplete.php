24040634642b4e244cea20497c352ab9106bb209
Diogo Oliveira de Melo <dmelo87@gmail.com>
2013-12-08 14:10:38 -0200

    help checking if autocomplete is working

---
+++ scripts/check_autocomplete.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once '../scripts/lib.php';
+
+$r = file_get_contents('http://amuzi.me/autocomplete.php?q=coldpl');
+$data = json_decode($r);
+$logger = Zend_Registry::get('logger');
+
+if (count($data) === 11) {
+    $logger->info('Autocomplete OK');
+} else {
+    $logger->err('Autocomplete ERROR');
+    $mail = new DZend_Mail('UTF-8');
+    $mail->setBodyText('Autocomplete is not working');
+    $mail->setFrom('support@amuzi.net', 'Diogo Oliveira de Melo');
+    $mail->addTo('dmelo87@gmail.com', 'Diogo Oliveira de Melo');
+    $mail->send();
+}
+
