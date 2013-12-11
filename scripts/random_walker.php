8e8e42f79b4daa9e3c892fea39af1c1c77f3e5b7
Diogo Oliveira de Melo <dmelo87@gmail.com>
2013-12-10 23:55:19 -0200

    fix html errors on main login page

--- scripts/random_walker.php
+++ scripts/random_walker.php
@@ -10,7 +10,7 @@ while (1) {
     $dom->loadHTML($content);
     $xpath = new DomXpath($dom);
     $links = $xpath->query('//*[@class="item-square playlist-square object-playlist"]');
-    $i = rand(0, $links->length + 1);
+    $i = rand(0, $links->length);
     echo $links->length . ' links found' . PHP_EOL;
     $a = $links->item($i)->getElementsByTagName('a');
     $a = $a->item(0);
