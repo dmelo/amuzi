b2ace22174beda6fb430a96dc7d42684e90009a8
Diogo Oliveira de Melo <dmelo87@gmail.com>
2014-07-19 23:41:59 +0000

    fix autocomple

--- scripts/env.php
+++ scripts/env.php
@@ -22,6 +22,7 @@
  */
 
 
+require_once '../vendor/autoload.php';
 // Define path to application directory
 defined('APPLICATION_PATH')
     || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
