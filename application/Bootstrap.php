f8742e09a51cf39ade0218dd7918f27e03c2fe35
Diogo Oliveira de Melo <dmelo87@gmail.com>
2015-02-25 00:38:25 +0000

    Fix Bootstrap to load _initConfig at proper time

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -193,7 +193,9 @@ class Bootstrap extends DZend_Application_Bootstrap_Bootstrap
     {
         $this->bootstrap('path');
         $this->bootstrap('logger');
+        $this->bootstrap('config');
         $this->bootstrap('db');
+        $this->bootstrap('bootstrap');
         $auth = Zend_Auth::getInstance();
         $lang = 'en_US';
         if ($auth->hasIdentity()) {
